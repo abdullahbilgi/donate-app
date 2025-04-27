ddaac0c9974d521e1c19cfe9ca617377ce38a0ee
Ahmet Karakullukçu <karakullukcuahmetarif@gmail.com>
2025-04-27 17:30:16 +0300

    cloudname,apikey,apisecret application propertiese eklendi

--- donate-rest/src/main/java/com/project/donate/Config/CloudinaryConfig.java
+++ donate-rest/src/main/java/com/project/donate/Config/CloudinaryConfig.java
@@ -1,6 +1,7 @@
 package com.project.donate.Config;
 
 import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,12 +10,19 @@ import java.util.Map;
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${cloudinary.cloud-name}")
+    private String cloudName;
+    @Value("${cloudinary.api-key}")
+    private String apiKey;
+    @Value("${cloudinary.secret-key}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
         return new Cloudinary(Map.of(
-                "cloud_name", "dxa6obkhc",
-                "api_key", "628987816848172",
-                "api_secret", "rHHpyoRpUdxxYE_qnFrlwd9rMsI"
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret
         ));
     }
 }
